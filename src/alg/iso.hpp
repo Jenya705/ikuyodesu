38e3b33c8a9a7ddb4f5c8224968d4532c4b013e5
Jenya705 <51133999+Jenya705@users.noreply.github.com>
2025-07-13 11:31:19 +0200

    more linalg types

---
+++ src/alg/iso.hpp
@@ -0,0 +1,22 @@
+#ifndef ISO_HPP_INCLUDED
+#define ISO_HPP_INCLUDED
+
+#include "alg/vec.hpp"
+#include "alg/mat.hpp"
+
+class Iso2 {
+public:
+    Vec2<float> t;
+    Mat2x2<float> rs;
+
+    Iso2(Vec2<float> t, Mat2x2<float> rs): t(t), rs(rs) {}
+    Iso2(): Iso2(vec((float)0,(float)0), Mat2x2<float>()) {}
+    Iso2(Vec2<float> t): Iso2(t, Mat2x2<float>()) {}
+    Iso2(Mat2x2<float> rs): Iso2(vec((float)0, (float) 0), rs) {}
+
+    void scale(float s) { rs *= s; }
+    void rotate(float a) { rs *= rot_mat(a); }
+    Vec2<float> apply(Vec2<float> p) { return rs * (p - t); }
+};
+
+#endif
---
+++ src/camera.hpp
@@ -0,0 +1,11 @@
+#ifndef CAMERA_HPP_INCLUDED
+#define CAMERA_HPP_INCLUDED
+
+#include "alg/iso.hpp"
+
+class Camera {
+public:
+    Iso2 iso;
+};
+
+#endif
